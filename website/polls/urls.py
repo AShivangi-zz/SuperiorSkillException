f2eb7ef63ba6217de92d6d177ffced390576ad2d
Tyler <tyler.bruno@sjsu.edu>
2017-09-18 12:15:55 -0700

    Started new project for the master branch. This is the beginning of our actual project.

--- website/polls/urls.py
+++
@@ -1,9 +0,0 @@
-"""The URL declarations for this Django project; a “table of contents” of your Django-powered site. """
-
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-]
