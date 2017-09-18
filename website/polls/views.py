1a6012849ac5f749ed45a11d23443bcbbc8ff56a
Tyler <tyler.bruno@sjsu.edu>
2017-09-18 12:03:42 -0700

    Added comments

--- website/polls/views.py
+++ website/polls/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
-from django.template import loader
-
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
-
+from .models import Choice, Question
 
 """Each view is responsible for doing one of two things: returning an HttpResponse object containing 
 the content for the requested page, or raising an exception such as Http404. The rest is up to you."""
@@ -15,27 +14,71 @@ It can use a template system such as Django’s – or a third-party
  output XML, create a ZIP file on the fly, anything you want, 
  using whatever Python libraries you want."""
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    """The render() function takes the request object as its first argument, 
-    a template name as its second argument and a dictionary as its optional 
-    third argument. It returns an HttpResponse object of the given template rendered with the given context."""
-    return HttpResponse(template.render(context, request))
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        """
+        request.POST is a dictionary-like object that lets you access submitted data by key name. 
+        In this case, request.POST['choice'] returns the ID of the selected choice, as a string. 
+        request.POST values are always strings.
+        
+        request.POST['choice'] will raise KeyError if choice wasn’t provided in POST data.
+         The above code checks for KeyError and redisplays the question form with an error message 
+         if choice isn’t given.
+        """
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        """
+        After incrementing the choice count, the code returns an HttpResponseRedirect rather than a
+         normal HttpResponse. HttpResponseRedirect takes a single argument:
+          the URL to which the user will be redirected 
+          (see the following point for how we construct the URL in this case).
+
+        As the Python comment above points out, you should always return an HttpResponseRedirect 
+        after successfully dealing with POST data. This tip isn’t specific to Django; 
+        it’s just good Web development practice.
+        """
+
+        """
+        We are using the reverse() function in the HttpResponseRedirect constructor in this example.
+         This function helps avoid having to hardcode a URL in the view function. 
+        It is given the name of the view that we want to pass control to and the variable portion 
+        of the URL pattern that points to that view. In this case, using the URLconf we set up in Tutorial 3,
+         this reverse() call will return a string like
+
+        '/polls/3/results/'
+        where the 3 is the value of question.id. This redirected URL will then call the 
+        'results' view to display the final page.
+        """
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
