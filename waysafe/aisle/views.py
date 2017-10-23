f00e1b1fcd9c50cfa775b5585d920602fa64739a
Tyler <tyler.bruno@sjsu.edu>
2017-10-23 13:10:26 -0700

    Added a base_navBar and base_footer file. and implemented the new index page in index. however still having some small issues with index and the first background.png photo.

--- waysafe/aisle/views.py
+++ waysafe/aisle/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 # Create your views here.
 def index(request): #always put in request
-    return HttpResponse("<h1>This is the WaySafe app aisle</h1>")
+    template_name = 'aisle/index.html'
+    return render(request,template_name)
 
 def aisle(request):
 
