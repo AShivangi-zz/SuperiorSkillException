81ef53c777ba0c8b56c613540899a1712c338122
Shivi <shivi.2096@gmail.com>
2017-11-01 11:10:10 -0700

    register form and db edited

--- waysafe/homepage/models.py
+++ waysafe/homepage/models.py
@@ -11,6 +11,7 @@ It's how you are going to store data for your app.
 class customer(models.Model):
     user_name = models.CharField(max_length = 40, default="NULL")
     street_address = models.CharField(max_length = 40)
+    city = models.CharField(max_length = 40, default="City")
     state = models.CharField(max_length=20)
     zipcode = models.FloatField()
     password = models.CharField(max_length = 20)
