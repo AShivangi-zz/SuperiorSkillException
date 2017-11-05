8e0bfa090f1028f66ce1d95b1831a5f6c45b941b
avanibhatnagar <avanibhatnagar07@gmail.com>
2017-11-05 12:55:07 -0800

    login required for shop by history!

--- waysafe/history/views.py
+++ waysafe/history/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 # Create your views here.
+@login_required(login_url='/register/')
 def index(request): #always put in request
     template_name = 'history/index.html'
     return render(request,template_name)
