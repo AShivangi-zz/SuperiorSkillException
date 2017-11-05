e179f55f709f0f204c822857a370f9942c5aa7dd
Tyler <tyler.bruno@sjsu.edu>
2017-11-05 12:34:55 -0800

    Added a register app. Added cart app.

---
+++ waysafe/register/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+#from homepage.models import customer
+#from aisle.models import item
+
+
+# # Create your models here.
+# class history(models.Model):
+#     last_order = models.CharField(max_length=40, default='NULL')
+#     #user_name = models.ForeignKey(customer, on_delete=models.CASCADE, default='NULL')
+#     #last_order = models.ForeignKey(order, on_delete=models.CASCADE, default='NULL')
+#     # previous orders
+#
+#    # def __str__(self):
+#       #  return self.user_name + " " + self.last_order
+#
+#
+# class order(models.Model):
+#     order_name = models.CharField(max_length=40, default='NULL')
+#     #user_name = models.ForeignKey(customer, on_delete=models.CASCADE, default='NULL')
+#     #itemName = models.ForeignKey(item.item_name, on_delete=models.CASCADE, default='NULL')
+#     #price = models.ForeignKey(item.price, on_delete=models.CASCADE, default='NULL')
