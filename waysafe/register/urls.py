5345b1a5bdf42ca74669bfe7214a24640ea90262
avanibhatnagar <avanibhatnagar07@gmail.com>
2017-11-05 13:12:42 -0800

    sign up updated

--- waysafe/register/urls.py
+++ waysafe/register/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     #^$ means this is the default homepage page of the section..
     #/homepage/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.register_view, name='register'),
     # url(r'^lastOrder/$', views.lastOrder, name='lastOrder'),
     # url(r'^previousOrders/$', views.previousOrders, name='previousOrders'),
     # url(r'^savedItems/$', views.savedItems, name='savedItems'),
--- waysafe/register/views.py
+++ waysafe/register/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .forms import  UserRegisterForm
+
 # Create your views here.
 # Create your views here.
 def index(request): #always put in request
-    template_name = 'register/index.html'
+    template_name = 'index/login.html'
     return render(request , template_name)
 
+def register_view(request):
+    title = "Register"
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        user=form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
 
+        new_user = authenticate(username=user.username, password=password)
+        login(request, user)
+        return redirect("/aisle")
+
+    context ={
+        "form": form,
+        "title": title,
+    }
+    return render(request, "index/login.html", context)
 
