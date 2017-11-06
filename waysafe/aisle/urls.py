f48ea55c99b85b6821b05c92787139d65eca588c
Hedi Moalla <hedimoalla@gmail.com>
2017-11-06 12:48:07 -0800

    adding all aisles

--- waysafe/aisle/urls.py
+++ waysafe/aisle/urls.py
@@ -6,6 +6,13 @@ urlpatterns = [
     #/aisle/
     url(r'^$', views.index, name='index'),
     url(r'^babycare/$', views.babycare_item, name='babycare'),
-    url(r'^beverages/$', views.beverage_item, name='beverages')
+    url(r'^beverages/$', views.beverage_item, name='beverages'),
+url(r'^frozen/$', views.frozen_item, name='frozen'),
+url(r'^snacks/$', views.snacks_item, name='snacks'),
+url(r'^alcohol/$', views.alcohol_item, name='alcohol'),
+url(r'^dairy/$', views.dairy_item, name='dairy'),
+url(r'^meat/$', views.meat_item, name='meat'),
+url(r'^pasta/$', views.pasta_item, name='pasta')
+
     # url(r'^aisle/$', views.aisle, name='aisle'),
 ]
--- waysafe/aisle/views.py
+++ waysafe/aisle/views.py
@@ -23,3 +23,39 @@ def beverage_item(request):
     template_name = 'aisle/beverages.html'
     context = {"beverageitems": beverageitems}
     return render(request, template_name, context)
+
+def meat_item(request):
+    meatitems = Item.objects.raw('SELECT * FROM aisle_item WHERE category = "Meat"')
+    template_name = 'aisle/meat.html'
+    context = {"meatitems": meatitems}
+    return render(request,template_name, context)
+
+def frozen_item(request):
+    frozenitems = Item.objects.raw('SELECT * FROM aisle_item WHERE category = "Frozen"')
+    template_name = 'aisle/frozen.html'
+    context = {"frozenitems": frozenitems}
+    return render(request,template_name, context)
+
+def pasta_item(request):
+    pastaitems = Item.objects.raw('SELECT * FROM aisle_item WHERE category = "Pasta"')
+    template_name = 'aisle/pasta.html'
+    context = {"pastaitems": pastaitems}
+    return render(request,template_name, context)
+
+def dairy_item(request):
+    dairyitems = Item.objects.raw('SELECT * FROM aisle_item WHERE category = "Dairy"')
+    template_name = 'aisle/dairy.html'
+    context = {"dairyitems": dairyitems}
+    return render(request,template_name, context)
+
+def snacks_item(request):
+    snacksitems = Item.objects.raw('SELECT * FROM aisle_item WHERE category = "Snacks"')
+    template_name = 'aisle/snacks.html'
+    context = {"snacksitems": snacksitems}
+    return render(request,template_name, context)
+
+def alcohol_item(request):
+    alcoholitems = Item.objects.raw('SELECT * FROM aisle_item WHERE category = "Alcohol"')
+    template_name = 'aisle/alcohol.html'
+    context = {"alcoholitems": alcoholitems}
+    return render(request,template_name, context)
