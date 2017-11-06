a0b0702a9dacede8b8dadbe7f4dc505b5535e65b
Tyler <tyler.bruno@sjsu.edu>
2017-11-06 10:41:01 -0800

    Fixed payment. You are welcome shivangi

--- waysafe/payment/views.py
+++ waysafe/payment/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request): #always put in request
-    template_name = 'payment/templates/index.html'
+    template_name = 'payment/index.html'
     return render(request,template_name)
