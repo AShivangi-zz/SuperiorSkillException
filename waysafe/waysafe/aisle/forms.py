fe892b70a43b4f834af95c6d06e2fe4c61907dd0
lhy2016 <lhy920104@gmail.com>
2017-11-12 18:42:17 -0800

    Revert "Merge branch 'DevelopmentBranch' of https://github.com/AShivangi/SuperiorSkillException into DevelopmentBranch"
    This reverts commit b06b2e6bd7f3f980b6ef850c6c84d7ae7d4c1013, reversing
    changes made to baac8e0082ab3b3f21ae137107004cad9c59dbb9.

--- waysafe/waysafe/aisle/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.contrib.auth.models import User
-from .models import item
-
-class ItemForm(forms.ModelForm):
-    class Meta:
-        model = item
-        fields = ['item_name','catagory','sub_category','item_logo', 'price']
--- waysafe/waysafe/homepage/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-"""
-apps.py is basically the settings file for that app. 
-remember that app is basically the "page".  like the homepage page is an app
-"""
-
-class HomeConfig(AppConfig):
-    name = 'homepage'
