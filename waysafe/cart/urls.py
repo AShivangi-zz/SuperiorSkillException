2edc26d36c8d1828de5721eb52bd83239db61e56
mannyinnercity415 <talk2cestoni@yahoo.com>
2017-11-25 15:26:28 -0800

    Update urls file with delete action

--- waysafe/cart/urls.py
+++ waysafe/cart/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     #^$ means this is the default homepage page of the section..
     #/homepage/
     url(r'^$', views.index, name='index'),
+    url(r'^delete/(?P<item_id>\d+)/$', views.delete_item, name='delete')
     # url(r'^lastOrder/$', views.lastOrder, name='lastOrder'),
     # url(r'^previousOrders/$', views.previousOrders, name='previousOrders'),
     # url(r'^savedItems/$', views.savedItems, name='savedItems'),
